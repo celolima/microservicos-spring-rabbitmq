8a14d08f1bf4e6bf257b5870b638ae6dff5b8609
Marcelo Lima <28361274+celolima@users.noreply.github.com>
2025-04-16 16:15:33 -0300

    Implementa o serviço de envio de mensagem via SMS

--- notifacao-app/src/main/java/com/marcelo/notificacao/domain/Proposta.java
+++ notifacao-app/src/main/java/com/marcelo/notificacao/domain/Proposta.java
@@ -11,8 +11,6 @@ public class Proposta {
     private Double valorSolicitado;
     private Integer prazoPagamento;
     private Boolean aprovada;
-    private Boolean integrada;
     private String observacao;
     private Usuario usuario;
-
 }
